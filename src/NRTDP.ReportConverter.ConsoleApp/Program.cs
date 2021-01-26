1558e24ef68ef8d41c8f5d6b4bc2933b600145d3
Michael Hollas <mah6309@ads.northwestern.edu>
2021-01-26 10:18:20 -0600

    added FDR change to consoleapp

--- src/NRTDP.ReportConverter.ConsoleApp/Program.cs
+++ src/NRTDP.ReportConverter.ConsoleApp/Program.cs
@@ -13,7 +13,7 @@ namespace NRTDP.tdReportConverter.ConsoleApp
             var sw = new Stopwatch();
             sw.Start();
 
-            MzidmlWriter.ConvertToSeperateCompressedMzId(args[0], args[1], 0.05);
+            MzidmlWriter.ConvertToSeperateCompressedMzId(args[0], args[1], args[2]);
           
             
             Console.WriteLine(sw.Elapsed);
