bfdea8b37ce36db93dc64a90cfd5c51d0476456a
mikehollas123 <hollas@gmail.com>
2025-08-27 10:15:54 -0500

    added progress removed uniprotID

--- src/NRTDP.ReportConverter.ConsoleApp/Program.cs
+++ src/NRTDP.ReportConverter.ConsoleApp/Program.cs
@@ -10,7 +10,9 @@ namespace NRTDP.tdReportConverter.ConsoleApp
             var sw = new Stopwatch();
             sw.Start();
 
-            MzidmlWriter.ConvertToSeperateCompressedMzId(args[0], args[1], Convert.ToDouble(args[2]));
+            IProgress<double> progress = new Progress<double>( x=> Console.WriteLine($"{(x * 100):N2}"));
+            //MzidmlWriter.ConvertToSeperateCompressedMzId(args[0], args[1], Convert.ToDouble(args[2]));
+            MzidmlWriter.ConvertToSeperateMzId(@"D:\Seer Dataset\2024-03-14-22-26-11_codeset_6_0_0.tdReport", @"D:\Seer Dataset", 0.01, progress);
 
             Console.WriteLine(sw.Elapsed);
         }
