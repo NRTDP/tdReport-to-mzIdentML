c57d2b791e1c549a3b81b6249cca03ca77abef37
mikehollas123 <63059466+mikehollas123@users.noreply.github.com>
2025-08-27 10:26:21 -0500

    turned into a console app

--- src/NRTDP.ReportConverter.ConsoleApp/Program.cs
+++ src/NRTDP.ReportConverter.ConsoleApp/Program.cs
@@ -10,9 +10,22 @@ namespace NRTDP.tdReportConverter.ConsoleApp
             var sw = new Stopwatch();
             sw.Start();
 
-            IProgress<double> progress = new Progress<double>( x=> Console.WriteLine($"{(x * 100):N2}"));
+            IProgress<double> progress = new Progress<double>(x => Console.WriteLine($"{(x * 100):N2}"));
             //MzidmlWriter.ConvertToSeperateCompressedMzId(args[0], args[1], Convert.ToDouble(args[2]));
-            MzidmlWriter.ConvertToSeperateMzId(@"D:\Seer Dataset\2024-03-14-22-26-11_codeset_6_0_0.tdReport", @"D:\Seer Dataset", 0.01, progress);
+            if ((args.Length == 1 && args[0] == "-h") || args.Length == 0)
+            {
+                Console.WriteLine("Usage: <TDreportPath> <outputFolder (optional)> <FDR (optional defualt:0.01 i.e. 1%)>");
+            }
+            if (args.Length == 3)
+                MzidmlWriter.ConvertToSeperateMzId(args[0], args[1], Convert.ToDouble(args[2]), progress);
+            else if (args.Length == 2)
+                MzidmlWriter.ConvertToSeperateMzId(args[0], args[1], 0.01, progress);
+            else if (args.Length == 2)
+            {
+                MzidmlWriter.ConvertToSeperateMzId(args[0], args[1], 0.01, progress);
+            }
+            else
+                throw new ArgumentException("There must be between 1-3 arguments.");
 
             Console.WriteLine(sw.Elapsed);
         }
